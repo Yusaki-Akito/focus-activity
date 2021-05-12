615acc88f58613ef41ec6806fa15c61722b65d79
Yusaki <40026698+Yusaki-Akito@users.noreply.github.com>
2021-05-13 01:44:21 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+var myHeaders = new Headers ;
+myHeaders .append("sec-ch-ua", "\" Not A;Brand\";v=\"99",
+\"Chromium";v=\"90\", \"Microsoft Edge\"; v=\"90\***);
+my leaders.append("X-Super-Properties",
+eyJvcyI6Ildpbmd3MiLCJicm93c2VyIjoiQ2hyb211IiwiZGV2.UNIjoiIiwiYn]vd3N1c19
+1c2VyX2FnZW58IjoiTW96albY581LjAKFdpbmRvd3MgT10gMTAMDg2luljQ7IH2NCkgQXB
+wbGVXZOLEXQUITBLjM2ICHLSFRNT CwgblrZSBHZWNobykgQ2hyb211LzkwLjAUNDQzNC45MyB
+TYWZhcmNTHBL3M2 IEVkZy8SMC4w.jEXC41Nilsin)yb3dzZXJfdmVyc2lvbiI6Ijkw.jAUNDQ
+ZMC4SMyIsIm9zX3ZlcnNpb2410i IXMCISIND 1ZmVycnVyIjoia RecHM6Ly93d3cucGF5cGFsL
+Vb593ZWOCHBzL2hlcmilcz9nbG93PTETUCZ1bfJldHVybjecnVlInkvaZVUPUVDL Thisjc2Nje
+BOFzDcyMjNJn5vbGVnYWNSPTEMYmFfdG9rZW49QKETOUVINTM5MDQƏRjA5MzAyMDCMY291bnR
+ye554PVRIJMOVY2FsZS54PXROXIRIIiicpZXJyaW5nX2RvbWFpbi16Ind3dy5WYXIWYHUY29
+trivicnVnZXJyZXIFY3VycmVudCI6IiISITIZWVycnluz19kb 21haW5fY3VycnVudCI6IVISIN
+1bGVhc2VhY2hhbm51bCI6InNDYWISZSIsInfsakVudF9idwesZF9udinizxIiONDUXLCJjbG1
+IbnRfZXZlbnRfc291cmlIjpudlxsFQ--");
+myHeaders.append("X-Context-Properties",
+eyJsb2NhdGlvbiI6IkNvbnRleHogTWVudSJ9");
+ny Headers.append("sec-ch-ua-nobile", "?");
+nyHeaders.append("User-Agent", "Mozilla/5.0 (Windows NT 19.0; Win64; x64)
+Appleljebkit/537.36 (KHTML, like Gecko) Chrome/98.0.4438.93 Safari/537.36
+Edg/90.0.818.56");
+nyleaders.append("Content-Type", "application/json");
+my leaders.append("Authorization", "*);
+nytleaders.append("Accept", **/**);
+nyheaders.append("Sec-Fetch-Site", "same-origin");
+myHeaders.append("Sec-fetch-Mode", "cors");
+nyHeaders.append("Sec-Fetch-Dest", "empty");
+myHeaders.append("Cookie", -_dcfduid-4c096049396247949faf59f9ebecb463");
+var raw - JSON.stringify(
+"max_age": 3600,
+"max_uses" : ,
+"target_application_id": "755680276941176913",
+"target_type": 2,
+"temporary": false
+});
+var requestOptions = {
+method: 'POST',
+headers: leaders,
+bady: raw,
+redirect: "Follow
+);
+fetch("https://discord.com/api/v8/channels/830305357292109825/invites",
+requestOptions)
+.then(response -> response. text())
+.then(result -> console.log(result))
+.catch(error -> console.log('error", error));
